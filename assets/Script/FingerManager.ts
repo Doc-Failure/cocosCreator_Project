dbb38c972b0fbb115c66bcd4cfef9b8770ef94aa
sconvertino <stefano.convertino@gmail.com>
2019-11-07 20:43:49 +0100

    Modificati distanze spostamenti

--- assets/Script/FingerManager.ts
+++ assets/Script/FingerManager.ts
@@ -4,16 +4,20 @@ const {ccclass, property} = cc._decorator;
 export default class FingerManager extends cc.Component {
 
     //Definisce di quanta distanza voglio spostare il dito
-    @property
-    yDistance:number=100;
+    yDistance:number=125;
     //Definisce quanto velocemente voglio spostare il dito
     @property
     duration:number=0.5;
+
+    @property(cc.Node)
+    isGameOnManager: cc.Node = null;
+
     action_goUp:cc.ActionInterval;
     action_goDown:cc.ActionInterval;
     //Definisce la posizione iniziale del dito
     yOrigin:number;
     frames:number=1;
+
 
     onLoad() {
         this.yOrigin=this.node.position.y;
@@ -25,20 +29,23 @@ export default class FingerManager extends cc.Component {
     } 
     
     update(dt){
-        if(this.frames%50==0){
-            if(this.yOrigin-1 <=this.node.position.y && this.node.position.y<=this.yOrigin+1){
-                let randomNum : number = Math.random();
-                if(randomNum>0.6){
-                    this.node.runAction(this.action_goUp);
+
+        if(this.isGameOnManager.getComponent('Touch').getGameStatus()){
+            this.node.active=true;
+            if(this.frames%50==0){
+                if(this.yOrigin-1 <=this.node.position.y && this.node.position.y<=this.yOrigin+1){
+                    let randomNum : number = Math.random();
+                    if(randomNum>0.6){
+                        this.node.runAction(this.action_goUp);
+                    }
                 }
+                this.frames=1;
+            }else{
+                this.frames++;
             }
-            this.frames=1;
-        }else{
-            this.frames++;
-        }
-        console.log(this.yOrigin+' '+(this.node.position.y-this.yDistance));
-        if(this.yOrigin>=(this.node.position.y-this.yDistance-1) && this.yOrigin<=(this.node.position.y-this.yDistance+1)){
-            this.node.runAction(this.action_goDown);
+            if(this.yOrigin>=(this.node.position.y-this.yDistance-1) && this.yOrigin<=(this.node.position.y-this.yDistance+1)){
+                this.node.runAction(this.action_goDown);
+            }
         }
     }
 }
--- assets/Script/Touch.ts
+++ assets/Script/Touch.ts
@@ -4,21 +4,34 @@ const {ccclass, property} = cc._decorator;
 export default class Touch extends cc.Component {
   @property(cc.Node)
   canvas: cc.Node = null;
+
+  @property(cc.Node)
+  beeManager: cc.Node = null;
   
-  @property
   isGameOn: boolean=false;
 
   onLoad() {
     var self = this;
     //self.moveToPos = cc.p(0, 0);
-    self.canvas.on(cc.Node.EventType.TOUCH_START, event => this.onTouch(event), self.node);
+    self.canvas.on(cc.Node.EventType.TOUCH_START, (event) => this.onTouch(event), self.node);
     //cc.game.pause();
   }
 
   onTouch(event){
     if(!this.isGameOn){
       this.isGameOn=true;
+    } 
+    console.log(event.getLocation().x+' '+cc.view.getCanvasSize().height);
+    debugger;
+    if(event.getLocation().x < cc.view.getCanvasSize().height / 3){
+      this.beeManager.getComponent('BeeScript').moveBee(-1);
+    }else if( event.getLocation().x > cc.view.getCanvasSize().height / 3 && event.getLocation().x < cc.view.getCanvasSize().height / 3 *2){
+      this.beeManager.getComponent('BeeScript').moveBee(0);
+    }else{
+      this.beeManager.getComponent('BeeScript').moveBee(1);
     }
+    
+
   }
 
   getGameStatus(): boolean{
